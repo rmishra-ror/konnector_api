c4124ae73fc0d7ca355536c1c64302b71990de7c
Rahul Mishra <rahulmishra@Rahuls-MacBook-Pro.local>
2024-07-16 19:55:34 +0530

    seed users data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,22 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+User.create!(
+  [
+    {
+      id: 1,
+      name: "John",
+      email: "john@gmail.com",
+      campaigns_list: [
+        { campaign_name: "cam1", campaign_id: "id1" },
+        { campaign_name: "cam2", campaign_id: "id2" }
+      ]
+    },
+    {
+      id: 2,
+      name: "Jane",
+      email: "jane@gmail.com",
+      campaigns_list: [
+        { campaign_name: "cam1", campaign_id: "id1" },
+        { campaign_name: "cam3", campaign_id: "id3" }
+      ]
+    }
+  ]
+)
